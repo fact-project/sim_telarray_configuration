7dce4706f3c0533bc6b6ddb63a23d5f8f98be720
Noah Biederbeck <nbiederbeck@tutanota.com>
2020-07-01 15:10:16 +0200

    use environment.sh

--- dark-run.sh
+++ dark-run.sh
@@ -1,23 +1,20 @@
 #!/bin/bash
 
-simtel="../simtel/sim_telarray"
-data="../data"
-data_file="../data/Corsika/76900/epos_urqmd_iact_lapalma_winter/gamma/00031000/corsika_gamma_run_00031718_az010-020_zd11-12.eventio.zst"
-output_path="build/"
+source environment.sh
+
+data_file="../data/corsika/76900/epos_urqmd_iact_lapalma_winter/gamma/00031000/corsika_gamma_run_00031718_az010-020_zd11-12.eventio.zst"
 output_file="dark-simtel-output.zst"
 
 n_events=10
 n_photons=4
 
-
 az=155
 zd=11.5
 
-echo "removing ${output_path} instead of overwriting"
-rm ${output_path}/${output_file} -fv
+rm ${output_directory}/${output_file} -fv
 
 
-${simtel}/sim_telarray \
+${simtel_binary} \
     -Ibuild -Iconfig \
     -C dark_events=${n_events} \
     -C laser_events=${n_events} \
@@ -26,6 +23,6 @@ ${simtel}/sim_telarray \
     -C telescope_azimuth=${az} \
     -c config/FACT.cfg \
     -C maximum_events=1 \
-    -o ${output_path}/${output_file} \
-    -h ${output_path}/hist \
-    -i ${data}/${data_file}
+    -o ${output_directory}/${output_file} \
+    -h ${output_directory}/${output_file}.hdata \
+    -i ${data_file}
