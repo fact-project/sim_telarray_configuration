ec5e65d762194d7aef5d2b772d08d226de07f9fc
Noah Biederbeck <nbiederbeck@tutanota.com>
2020-07-01 15:08:02 +0200

    use pointing options from CORSIKA

--- run-simtel.sh
+++ run-simtel.sh
@@ -1,17 +1,23 @@
 #!/bin/bash
 
-simtel_bin="../simtel/sim_telarray/sim_telarray"
-
-AZ=155
-ZD=11.5
+source environment.sh
 
 infile=$1
 outfile=$2
+logfile=${outfile}.log
+histfile=${outfile}.hdata
 
-${simtel_bin} \
+for k in $(${SIM_TELARRAY_PATH}/bin/extract_corsika_tel --header-only --only-telescopes 1 "${infile}"); do
+    echo "export $k";
+    export $k;
+done
+
+${simtel_binary} \
     -Ibuild -Iconfig \
-    -C telescope_theta=${ZD} \
-    -C telescope_phi=${AZ} \
+    -C telescope_theta="${CORSIKA_THETA:-0.0}" \
+    -C telescope_phi="${CORSIKA_PHI:-0}" \
     -c config/FACT.cfg \
+    -h ${histfile} \
     -o ${outfile} \
-    -i ${infile}
+    -i ${infile} \
+    | gzip > ${logfile}
