bb6b615544b724c9849db713a354a9d7f1d4eb3d
Noah Biederbeck <nbiederbeck@tutanota.com>
2020-07-01 15:31:34 +0200

    remove obsolete lines

--- environment.sh
+++ environment.sh
@@ -2,7 +2,5 @@
 
 SIM_TELARRAY_PATH="../simtel/sim_telarray"
 simtel_binary="${SIM_TELARRAY_PATH}/sim_telarray"
-data_directory="../data"
-output_directory="build"
-fact_tools_jar="../../Downloads/fact-tools-v1.1.3.jar"
+fact_tools_jar="${HOME}/Downloads/fact-tools-v1.1.3.jar"
 fact_tools_git_dir="${HOME}/Uni/fact-tools/"
