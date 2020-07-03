af685661a2b203e996dbe94046057916ae437359
Noah Biederbeck <nbiederbeck@tutanota.com>
2020-07-03 14:17:40 +0200

    download only if newer

--- downloads.mk
+++ downloads.mk
@@ -11,16 +11,16 @@ $(OUTPUT_PATH):
 	mkdir -p $@
 
 $(FACT_TOOLS): | $(OUTPUT_PATH)
-	wget \
+	wget --timestamping \
 		https://github.com/fact-project/fact-tools/releases/download/$(FACT_TOOLS_VERSION)/fact-tools-$(FACT_TOOLS_VERSION).jar \
 		-O $@
 
 $(OUTPUT_PATH)/testMcDrsFile.drs.fits.gz: | $(OUTPUT_PATH)
-	wget \
+	wget --timestamping \
 		https://github.com/fact-project/fact-tools/raw/master/src/main/resources/testMcDrsFile.drs.fits.gz \
 		-O $@
 
 $(OUTPUT_PATH)/delays_zero.csv: | $(OUTPUT_PATH)
-	wget \
+	wget --timestamping \
 		https://raw.githubusercontent.com/fact-project/fact-tools/master/src/main/resources/default/delays_zero.csv \
 		-O $@
