df00f2163af180b49b2ac15cf9ed57c0234d85fc
Noah Biederbeck <nbiederbeck@tutanota.com>
2020-07-10 16:18:29 +0200

    add 0.6d offset

--- run-simtel.sh
+++ run-simtel.sh
@@ -8,17 +8,19 @@ logfile=${outfile}.log
 histfile=${outfile}.hdata
 
 for k in $(${SIM_TELARRAY_PATH}/bin/extract_corsika_tel --header-only --only-telescopes 1 "${infile}"); do
-    echo "export $k";
     export $k;
 done
 
+offset=0.6
+
 ${simtel_binary} \
     -Ibuild -Iconfig \
-    -C telescope_theta="${CORSIKA_THETA:-0.0}" \
+    -C random_state=auto \
+    -C telescope_theta=$(echo "${CORSIKA_THETA:-0.0} + ${offset}" | bc -l) \
     -C telescope_phi="${CORSIKA_PHI:-0}" \
     -c config/FACT.cfg \
     -C show=all \
     -h ${histfile} \
     -o ${outfile} \
     -i ${infile} \
-    | gzip > ${logfile}
+    2>&1 | zstd > ${logfile}
